e21a49d3a2d251e116e36fc280ead4a3010f542b
Timur <t1m.1h@yandex.ru>
2022-04-28 21:28:45 +1200

    add Example 001

--- Lesson001/Example001_Three_Numbers/Program.cs
+++ Lesson001/Example001_Three_Numbers/Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает последнюю цифру этого числа. 
+// 456 -> 6 782 -> 2 918 -> 8
+
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int b = n % 10;
+Console.Write(b);
