42a7a234fe2e83496bb51b23a2b3242a6f87295d
Timur <t1m.1h@yandex.ru>
2022-04-28 22:00:36 +1200

    add Example 002 Max Min

---
+++ Lesson001/Example002_Max_Min/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 2. Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+Console.Write("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+int max = 0;
+int min = 0;
+
+if(a == b)
+{
+    Console.WriteLine("Числа равны");
+}
+else
+{
+    if(a > b)
+    { 
+        max = a;
+        min = b;
+        Console.WriteLine("Максимальное число: " + max);
+        Console.WriteLine("Минимальное число: " + min);
+    }
+    else
+    {
+        max = b;
+        min = a;
+        Console.WriteLine("Максимальное число: " + max);
+        Console.WriteLine("Минимальное число: " + min);
+    }
+}
