a9b74f93f836cbff0a7979e871870d5d1866f138
Timur <t1m.1h@yandex.ru>
2022-04-29 00:45:12 +1200

    add Example 003 Three Max

---
+++ Lesson001/Example003_Three_Max/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задача 4. Напишите программу, которая получает три числа и выдаёт максимальное из этих чисел.
+
+Console.Write("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите третье число: ");
+int c = Convert.ToInt32(Console.ReadLine());
+int max = a;
+if (max > b)
+{
+    if(max > c)
+    {
+        Console.Write("Максимальное число: " + max);
+    }
+    else 
+    {
+        max = c;
+        Console.Write("Максимальное число: " + max);
+    }
+}
+else
+{
+    max = b;
+    if(max > c)
+    {
+        Console.Write("Максимальное число: " + max);
+    }
+    else
+    {
+        max = c;
+        Console.Write("Максимальное число: " + max);
+    }
+}
