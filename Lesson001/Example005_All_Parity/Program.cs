faab704ea8f64f34f98d5d717239674b0bcd4cac
Timur <t1m.1h@yandex.ru>
2022-04-29 16:54:28 +1200

    add Example 005 All Parity

---
+++ Lesson001/Example005_All_Parity/Program.cs
@@ -0,0 +1,28 @@
+﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+Console.Write("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+int i = 0;
+
+while (i <= a)
+{
+    if(i%2!=0) // Проверка на нечётность
+    {
+        i++;
+    }
+    else
+    {
+    Console.Write(i + ", ");
+    i++;
+    }
+}
+// Второе решение без проверки на чётность.
+
+// Console.Write("Введите число: ");
+// int a = Convert.ToInt32(Console.ReadLine());
+// int i = 2;
+// 
+// while (i <= a)
+// {
+//    Console.Write(i + ", ");
+//   i+=2;
+// }
