7fd737592f65bb40e667ffe0b4272836aa69f30c
Timur <t1m.1h@yandex.ru>
2022-05-04 11:12:30 +0300

    add Example 004 Game Random Number

---
+++ Lesson002/Example004GameRandomNumber/Program.cs
@@ -0,0 +1,34 @@
+﻿// Угадайка.
+
+int random = new Random().Next(0, 101);
+Console.WriteLine("Игра - угадайте число от 0 до 100!");
+Console.Write("Press enter ");
+Console.ReadLine();
+Console.WriteLine("Введите желаемое количество попыток: ");
+int how = Convert.ToInt32(Console.ReadLine()); // Не вводить 0 :)
+Console.WriteLine();
+int count = 0;
+while (count < 101)
+{
+    count++;
+    Console.Write("Попытка: " + count + " ");
+    Console.Write("Ваше число: ");
+    int number = Convert.ToInt32(Console.ReadLine());
+    if (number > random) Console.WriteLine("Число больше загаданного");
+    if (number < random) Console.WriteLine("Число меньше загаданного");
+    if (number == random)
+    {
+        Console.WriteLine();
+        Console.WriteLine("Win!");
+        break;
+    }
+    if (count == how)
+    {
+        Console.WriteLine();
+        Console.WriteLine("Вы использовали все попытки.");
+        Console.WriteLine("Загаданное число: " + random);
+        break;
+    }
+    Console.WriteLine();
+}
+Console.WriteLine("Попыток: " + count);
