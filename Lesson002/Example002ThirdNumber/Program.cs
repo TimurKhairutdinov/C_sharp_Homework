ab87fee7f8ff3806a59088fa1c1f7b913168b942
Timur <t1m.1h@yandex.ru>
2022-05-04 09:05:15 +0300

    add Example 003 Week

---
+++ Lesson002/Example002ThirdNumber/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 13. Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+
+// string str = Convert.ToString(new Random().Next(0,100000));
+string numbers = Console.ReadLine();
+// Console.WriteLine(numbers);
+
+void ThirdNumber(string number) // 0 1 2 3 4 5 
+{
+    int index = 0;
+    string [] str = {};
+    while(index < 3)
+    {
+       // if (number[index] == str[0])
+        {
+            Console.Write("No");
+        }
+        index++;
+
+    }
+}
+// ThirdNumber(numbers);
+string[] nole = {};
+Console.Write(nole);
+// int index = 2;
+//if (numbers[index] == nole[0]) 
---
+++ Lesson002/Example003Week/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задача 15. Напишите программу, которая принимает на вход цифру, обозначающую день недели и проверяет является этот день выходным.
+
+Console.WriteLine("Введите число от 1 до 7 соответствующему дню недели");
+int day = Convert.ToInt32(Console.ReadLine());
+Week(day);
+
+void Week(int day)
+{
+    int monday = 1;
+    int tuesday = 2;
+    int wednesday = 3;
+    int thursday = 4;
+    int friday = 5;
+    int saturday = 6;
+    int sunday = 7;
+    if(day!=0)
+    {
+        if(day <= 7)
+        {
+            if (day == monday) Console.WriteLine("Понедельник"); 
+            if (day == tuesday) Console.WriteLine("Вторник"); 
+                if (day == wednesday) Console.WriteLine("Среда"); 
+            if (day == thursday) Console.WriteLine("Четверг"); 
+            if (day == friday) Console.WriteLine("Пятница"); 
+            if (day == saturday) Console.WriteLine("Суббота выходной"); 
+            if (day == sunday) Console.WriteLine("Воскресенье выходной"); 
+        }
+        else Console.Write("Введенное число больше 7");
+    }
+    else Console.WriteLine("Вы ввели 0");
+}
