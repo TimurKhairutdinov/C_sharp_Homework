97b9a6e8bbdf46e1456e034f760d4ca4bb8cb9f7
Timur <t1m.1h@yandex.ru>
2022-05-04 09:44:43 +0300

    correct example 002 third number

--- Lesson002/Example002ThirdNumber/Program.cs
+++ Lesson002/Example002ThirdNumber/Program.cs
@@ -1,26 +1,17 @@
 ﻿// Задача 13. Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
 
 
-// string str = Convert.ToString(new Random().Next(0,100000));
-string numbers = Console.ReadLine();
-// Console.WriteLine(numbers);
+string str = Convert.ToString(new Random().Next(0,10000)); // random
+string numbers = Console.ReadLine(); // input
 
-void ThirdNumber(string number) // 0 1 2 3 4 5 
-{
-    int index = 0;
-    string [] str = {};
-    while(index < 3)
-    {
-       // if (number[index] == str[0])
-        {
-            Console.Write("No");
-        }
-        index++;
-
-    }
+void ThirdNumber(string number)
+{                                                     
+    if (number.Length >= 3) Console.Write(number[2]); 
+    else Console.Write("Число состоит менее, чем из трёх цифр");
 }
-// ThirdNumber(numbers);
-string[] nole = {};
-Console.Write(nole);
-// int index = 2;
-//if (numbers[index] == nole[0]) 
+
+ThirdNumber(numbers);
+Console.WriteLine();
+Console.WriteLine();
+Console.WriteLine(str);
+ThirdNumber(str);
