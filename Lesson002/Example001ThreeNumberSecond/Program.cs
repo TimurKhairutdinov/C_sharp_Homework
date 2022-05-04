3c44148f96ef5c3440023943f761e37fe5d4cdb0
Timur <t1m.1h@yandex.ru>
2022-05-04 07:25:27 +0300

    add Lesson 002 Example 001

---
+++ Lesson002/Example001ThreeNumberSecond/Program.cs
@@ -0,0 +1,5 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру числа.
+
+string stringNumber = Console.ReadLine();
+
+Console.Write(stringNumber[1]);
