e5a9e1a8de14671675de96aa95a5e8406e2bf69f
Timur <t1m.1h@yandex.ru>
2022-05-06 16:13:38 +0300

    add lesson 003 Example 003 Exponentiation Table

---
+++ Lesson003/Example003_Exponentiation_Table/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 23. Напишите программу, которая принимает на вход число N и выдаёт таблицу кубов чисел от 1 до N.
+// Внес изменения: при вводе данных, нужно ввести степень возведения, таким образом можно ввести любую степень, не ограничаваясь условием.
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите сепень возведения: ");
+int exponent = Convert.ToInt32(Console.ReadLine()); // Позволяет ввести любую степень, а не только 3 как указано в условии.
+int count = 1;
+while (count <= number)
+{
+    int countQuatro = Convert.ToInt32(Math.Pow(count,exponent));
+    Console.WriteLine($"{count}^{exponent} = {countQuatro}");
+    count++;
+}
