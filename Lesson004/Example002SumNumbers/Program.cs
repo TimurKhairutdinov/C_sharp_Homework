5062f13e4204f64a1a8671da17562abf8b5b87e1
Timur <t1m.1h@yandex.ru>
2022-05-19 22:03:20 +0300

    add example 002 sum numbers

---
+++ Lesson004/Example002SumNumbers/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+int InputNumber(string name)
+{
+    Console.WriteLine($"Введите число {name}: ");
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number;
+}
+int number = InputNumber("");
+int result = 0;
+int remainder;
+while (number != 0)
+{
+    remainder = number % 10;    // 452%10 = 2 // 45%10 = 5 // 4%10 = 4
+    result += remainder;        // 0 = 0+2     // 2 = 2+5   // 7 = 7+4
+    number = number / 10;       // 452/10 = 45 // 45/10 = 4 // 4/10 = 0
+}
+Console.WriteLine($"Ответ: {result}");
