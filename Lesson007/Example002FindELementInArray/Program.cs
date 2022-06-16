c6c59103707ea5eb1c1afcd1d3f9a4c1c0ca8072
Timur.Khairutdinov <t1m.1h@yandex.ru>
2022-06-17 01:11:33 +0300

    add example 003 Create Int Array and FInd Arithmetical Mean In Columns

---
+++ Lesson007/Example002FindELementInArray/Program.cs
@@ -0,0 +1,80 @@
+﻿// Задача 50: Напишите программу, которая на вход принимает позиции элемента в двумерном массиве,
+//  и возвращает значение этого элемента или же указание, что такого элемента нет.
+
+// Первое решение, где за позицию элемента считается его позиция в таблице,
+// если считать по строке, а затем переходя на новую строку.
+// 1 2 3 4 5
+// 6 7 8 9 10
+
+int rows = 3;
+int columns = 5;
+int[,] table = new int[rows, columns];
+
+FillArray(table, 0, 100);
+PrintArray(table);
+
+Console.Write("Введите позицию элемента: ");
+int findPosition = Convert.ToInt32(Console.ReadLine());
+
+// Условие, если заданная позиция отсутствует в массиве.
+int sizeTable = rows * columns;
+if (findPosition > sizeTable)
+{
+    Console.WriteLine(
+        "Элемент не найден!" + " " +
+        "Позиция элемента находится за пределами массива.");
+}
+
+
+int currentPosition = 1;
+int result = 0;
+bool findStatus = false;
+// Перебор массива по циклам, до тех пор пока currentPosition не сравняется с findPosition
+for (int i = 0; i < table.GetLength(0); i++)
+{
+    for (int j = 0; j < table.GetLength(1); j++)
+    {
+        if (currentPosition != findPosition)
+        {
+            currentPosition++;
+        }
+        else
+        {   // Выход из цикла, когда позиция найдена.
+            result = table[i, j];
+            Console.WriteLine($"Элемент на позиции {findPosition} = {result}");
+
+            // Переменная для указания статуса поиска, нужна для выхода из следующего цикла.
+            findStatus = true;
+            break;
+        }
+    }
+    // Проверка статуса поиска, чтобы завершить цикл, если элемент найден.
+    if (findStatus != false)
+    {
+        break;
+    }
+}
+
+void FillArray(int[,] arr, int min, int max)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            arr[i, j] = new Random().Next(min, max);
+        }
+    }
+}
+
+void PrintArray(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write(arr[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
---
+++ Lesson007/Example003CreateIntArray&FindArithmeticalMeanInColumns/Program.cs
@@ -0,0 +1,50 @@
+﻿// Задача 52: Задайте двумерный массив из целых чисел. 
+// Найдите среднее арифметическое элементов в каждом столбце.
+
+int rows = 9;
+int columns = 9;
+
+int[,] table = new int[rows, columns];
+
+FillArray(table, 0, 10);
+PrintArray(table);
+
+string outputResult = string.Empty;
+for (int i = 0; i < table.GetLength(1); i++)
+{
+    double tempResult = 0;
+    for (int j = 0; j < table.GetLength(0); j++)
+    {
+        tempResult += table[j, i];
+    }
+
+    tempResult /= rows;
+    outputResult += Math.Round(tempResult, 2) + " ";
+}
+
+Console.WriteLine("Среднее арифметическое каждого столбца: " + outputResult);
+
+
+void FillArray(int[,] arr, int min, int max)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            arr[i, j] = new Random().Next(min, max);
+        }
+    }
+}
+
+void PrintArray(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write(arr[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
